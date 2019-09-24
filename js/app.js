9df83ab436903c934433bbd96ccfe84e98f27715
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-09-24 14:19:28 -0700

    finished final message to user based on questions answered correctly

--- js/app.js
+++ js/app.js
@@ -7,11 +7,14 @@ var userGreeting = alert('I hope you enjoy my game ' + user + '. Let\'s see if y
 
 // About Me Guessing Game
 
+var counter = 0;
+
 var snowBoarding = prompt('Do I like to snowboard?');
 
 if (snowBoarding.toUpperCase() === "YES" || snowBoarding.toUpperCase() === "Y") {
     // console.log('You got that right!')
     alert('You got that right!')
+    counter++;
 } else {
     // console.log('Unfortunately, that\'s not correct');
     alert('Unfortunately, that\'s not correct');
@@ -22,6 +25,7 @@ var coding = prompt('Do I like to code?');
 if (coding.toUpperCase() === "YES" || coding.toUpperCase() === "Y") {
     // console.log('You got that right!')
     alert('You got that right!')
+    counter++;
 } else {
     // console.log('Unfortunately, that\'s not correct');
     alert('Unfortunately, that\'s not correct');
@@ -32,6 +36,7 @@ var outdoors = prompt('Do I enjoy the outdoors?');
 if (outdoors.toUpperCase() === "YES" || outdoors.toUpperCase() === "Y") {
     // console.log('You got that right!')
     alert('You got that right!')
+    counter++;
 } else {
     // console.log('Unfortunately, that\'s not correct');
     alert('Unfortunately, that\'s not correct');
@@ -45,6 +50,7 @@ if (home.toUpperCase() === "YES" || home.toUpperCase() === "Y") {
 } else {
     // console.log('You got that right!')
     alert('You got that right!')
+    counter++;
 }
 
 var pets = prompt('Do I own any pets?');
@@ -55,4 +61,16 @@ if (pets.toUpperCase() === "YES" || pets.toUpperCase() === "Y") {
 } else {
     // console.log('You got that right!')
     alert('You got that right!')
-};
+    counter++;
+}
+
+// var finalMessage = alert('Awesome ' + user + ' You got ' + counter + ' out of 5 correct!');
+
+if (counter >= 3) {
+    alert('Awesome ' + user + '!' + ' You got ' + counter + ' out of 5 correct!')
+    alert('I guess you know a lot about me ' + user + '!')
+} else {
+    alert('Too bad ' + user + ',' + ' You only got ' + counter + ' out of 5 correct!')
+    alert('It\'s ok, ' + user + ' we have 5 months to know more about each other!')
+}
+

4f4d480385a324b39933decad3a0441dd1c33c7c
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-09-24 13:05:36 -0700

    commented out console.log messages. Replaced with alerts.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,53 @@
 var snowBoarding = prompt('Do I like to snowboard?');
 
 if (snowBoarding.toUpperCase() === "YES" || snowBoarding.toUpperCase() === "Y") {
+    console.log('You got that right!')
     alert('You got that right!')
 } else {
-    alert('Unfortunately, that\'s not correct');
+    console.log('Unfortunately, that\'s not correct');
+    // alert('Unfortunately, that\'s not correct');
 }
 
 var coding = prompt('Do I like to code?');
 
 if (coding.toUpperCase() === "YES" || coding.toUpperCase() === "Y") {
+    console.log('You got that right!')
     alert('You got that right!')
 } else {
+    console.log('Unfortunately, that\'s not correct');
     alert('Unfortunately, that\'s not correct');
 }
 
 var outdoors = prompt('Do I enjoy the outdoors?');
 
 if (outdoors.toUpperCase() === "YES" || outdoors.toUpperCase() === "Y") {
+    console.log('You got that right!')
     alert('You got that right!')
 } else {
+    console.log('Unfortunately, that\'s not correct');
     alert('Unfortunately, that\'s not correct');
 }
 
 var home = prompt('Do I currently live in Redmond?');
 
 if (home.toUpperCase() === "YES" || home.toUpperCase() === "Y") {
-    alert('You got that right!')
-} else {
+    
+
+    console.log('Unfortunately, that\'s not correct');
     alert('Unfortunately, that\'s not correct');
+} else {
+    console.log('You got that right!')
+    alert('You got that right!')
 }
 
 var pets = prompt('Do I own any pets?');
 
 if (pets.toUpperCase() === "YES" || pets.toUpperCase() === "Y") {
-    alert('You got that right!')
-} else {
+    console.log('Unfortunately, that\'s not correct');
     alert('Unfortunately, that\'s not correct');
+} else {
+    console.log('You got that right!')
+    alert('You got that right!')
+
+   
 }

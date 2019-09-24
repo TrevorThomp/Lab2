3f2079ae27422d89a06c8678072d9514f9174913
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-09-24 12:58:16 -0700

    completed user prompts

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var snowBoarding = prompt('Do I like to snowboard?');
+
+if (snowBoarding.toUpperCase() === "YES" || snowBoarding.toUpperCase() === "Y") {
+    alert('You got that right!')
+} else {
+    alert('Unfortunately, that\'s not correct');
+}
+
+var coding = prompt('Do I like to code?');
+
+if (coding.toUpperCase() === "YES" || coding.toUpperCase() === "Y") {
+    alert('You got that right!')
+} else {
+    alert('Unfortunately, that\'s not correct');
+}
+
+var outdoors = prompt('Do I enjoy the outdoors?');
+
+if (outdoors.toUpperCase() === "YES" || outdoors.toUpperCase() === "Y") {
+    alert('You got that right!')
+} else {
+    alert('Unfortunately, that\'s not correct');
+}
+
+var home = prompt('Do I currently live in Redmond?');
+
+if (home.toUpperCase() === "YES" || home.toUpperCase() === "Y") {
+    alert('You got that right!')
+} else {
+    alert('Unfortunately, that\'s not correct');
+}
+
+var pets = prompt('Do I own any pets?');
+
+if (pets.toUpperCase() === "YES" || pets.toUpperCase() === "Y") {
+    alert('You got that right!')
+} else {
+    alert('Unfortunately, that\'s not correct');
+}
